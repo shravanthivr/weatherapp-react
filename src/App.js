0f9c647b24594556e1a463b5f2b39c30b2aa982c
shravanthi <shravanthivr@gmail.com>
2019-07-31 22:19:34 -0400

    first commit
    first commit of weather-app that provides current location weather data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,148 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Fragment } from 'react';
 import './App.css';
+import axios from 'axios';
+import Geocode from "react-geocode";
+import WeatherInfo from './WeatherInfo/WeatherInfo';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+class App extends React.Component {
+    constructor (props) {
+        super (props);
+        this.state = {
+            inputString : '',
+            locArray : [],
+            address: '',
+            weatherInfoData : [],
+            isAppLoaded :false
+        };
+    }
+
+      //using geolocation
+    componentDidMount () {
+        if(navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition (
+                (position) => {
+                this.setState({
+                    locArray : [position.coords.latitude, position.coords.longitude],
+                }, this.notifyWeatherUpdate);
+            },
+            (error) => {console.log(error.message);
+            });
+        } else {
+            console.log ('Geolocation not available');
+        }    
+    } // end of componentDiDMount 
+
+    notifyWeatherUpdate = () => {
+        const haslocArray = this.state.locArray.length > 0;
+        const hasCity = (this.state.inputString !== '');  
+        const latlng =  `${this.state.locArray[0]},${this.state.locArray[1]}`;  
+        const key= 'AIzaSyAG0x5CSf-pR0Z1fZ6zRjtRSgbOz6lsoQ8';
+
+        if (haslocArray || hasCity) { 
+            this.fetchWeatherData(haslocArray).then (forecastData => {
+                this.setState ({
+                    weatherInfoData : this.extractDataForCurrentWeather(forecastData),
+                    isAppLoaded : true
+                })
+            }).catch (error => {
+                console.log ('Error', error);
+            });   
+            
+           // reverse geocoding     
+          return fetch(
+            `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latlng}&key=${key}`)
+              .then((res) => res.json())
+              .then((data) => {
+                if(data.status !== 'OK') {
+                  throw new Error ('Geocode error : ${data.status}');
+                 }
+               this.setState ({
+                address:  data.results[0].address_components[2].long_name + ',' + data.results[0].address_components[5].long_name,
+                isAppLoaded : true
+               });
+
+            });
+     
+        } // end of haslocalArray..
+    } // end of notifyweatherUpdate
+        
+
+    // fetching data
+    fetchWeatherData = (haslocArray)=> {
+        const API_KEY = 'e9ce53dc730b2080454872f4d62d454e'; // data sky key
+        const proxy = 'https://cors-anywhere.herokuapp.com/';
+        const BASE_URL = 'https://api.darksky.net/forecast';
+        const exclude = "?exclude=minutely,hourly,alerts,flags";
+        const queryParams = (haslocArray) ? `${this.state.locArray[0]},${this.state.locArray[1]}` : `q=${this.state.inputString}`;
+        const unitType = (this.state.unit === 'C') ? 'metric' : 'imperial';
+
+        const url = `${proxy}${BASE_URL}/${API_KEY}/${queryParams}${exclude}`;
+
+        return axios.get(url).then(response => {
+            return response.data;
+
+            }).catch (error => {
+            console.log ('error:',error);
+        })
+
+    }
+    
+    // Takes date object or unix timestamp in ms and returns day string
+    getDay = (time) => {
+        const daysNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday ", "Friday", "Saturday"];
+        return daysNames[(new Date(time).getDay())];
+    }
+
+
+    extractDataForCurrentWeather = (forecastData) => {
+        //console.log (forecastData);
+        const month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        const latitude = forecastData.latitude;
+        const longitude = forecastData.longitude;
+
+        const todayForecast = forecastData.currently;
+        const time = new Date(todayForecast.time * 1000);
+        const day = this.getDay(time);
+        const date = `${month[time.getMonth()]} ${time.getDate()}, ${time.getFullYear()}`;
+
+        const description = todayForecast.summary;
+        let mainTemperature = todayForecast.apparentTemperature;
+        const icon = todayForecast.icon;
+        const pressure = todayForecast.pressure;
+        const humidity = todayForecast.humidity;
+        const windSpeed = todayForecast.windSpeed;
+    
+        return {      
+          date,
+          time,
+          day,
+          description,
+          mainTemperature,
+          icon,
+          pressure,
+          humidity,
+          windSpeed
+        }
+    } // end of extractData
+
+
+
+    render () {
+        const instructionLayout = <div className="app-instruction">
+            <p>Allow Location Access  to get started.</p>
+        </div>
+
+        const mainAppLayout = <React.Fragment>
+            <h1 className="header"><div className="title">WeatherApp</div></h1>
+            <WeatherInfo weatherInfoData={this.state.weatherInfoData} address={this.state.address} />
+        </React.Fragment>
+        return (
+            <div className="app-container">
+                 {this.state.isAppLoaded ? mainAppLayout : instructionLayout}
+            </div>
+        );
+    } // end of render 
+} // end of App class
 
 export default App;
---
+++ src/WeatherInfo/WeatherInfo.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import ReactAnimatedWeather from 'react-animated-weather';
+import './WeatherInfo.css';
+import './weather-icons/css/weather-icons.min.css';
+
+
+class WeatherInfo extends React.Component {
+
+ render() {
+  const {   
+    date,
+    time,
+    day,
+    description,
+    mainTemperature,
+    icon, pressure,
+    humidity, windSpeed} = this.props.weatherInfoData;
+
+    const address = this.props.address;
+
+     // skyicons
+    const list  = [
+      'CLEAR_DAY',
+      'CLEAR_NIGHT',
+      'PARTLY_CLOUDY_DAY',
+      'PARTLY_CLOUDY_NIGHT',
+      'CLOUDY','RAIN',
+      'SLEET','SNOW',
+      'WIND','FOG'
+    ];
+    const defaults = {
+      icon: 'PARTLY_CLOUDY_DAY',
+      color: 'goldenrod',
+      size: 128,
+      animate: true
+    };
+    var iconName = icon !== undefined ? list[list.indexOf (icon.toUpperCase())] : defaults.icon;
+ 
+    return (
+      <div>
+        <div className = "location">
+          <span>{address}</span>
+        </div>
+        <div className = "weather-container">
+          <div className="date-container">
+              <div>{day}</div>
+              <div>{date}</div>
+          </div>
+          <div className="icon-container">
+              <ReactAnimatedWeather icon={iconName} color={defaults.color}
+                  size={defaults.size}
+                  animate={defaults.animate}
+              />
+              <div className="weather-desc">{description}</div>
+          </div>
+          <div className="temp-container">
+              <div className="temp-text">
+                <span>{mainTemperature}</span>
+                <i className="wi wi-degrees"></i>
+                <span>F</span>
+              </div>
+          </div>
+          <div className="extra-info-container">
+            <div className="extra-info-item">
+                <span><i className="wi wi-humidity"></i></span>
+                <span>Humidity </span>
+                <span>{humidity}%</span>
+            </div>
+            <div className="extra-info-item">
+                <span><i className="wi wi-barometer"></i></span>
+                <span>Pressure </span>
+                <span>{pressure} hPa</span>
+            </div>
+            <div className="extra-info-item">
+                <span><i className="wi wi-strong-wind"></i></span>
+                <span>Wind Speed</span>
+                <span>{windSpeed} </span>
+            </div>
+          </div>
+        </div>
+      </div >
+    );
+
+ }
+
+}
+
+export default WeatherInfo;
